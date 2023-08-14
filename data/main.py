1bfa1b0431fbf829e0932d7bd5ee7ab26bfd1671
Renato Silva <renatomate@gmail.com>
2023-08-13 22:24:37 -0300

    Inclusao da biblioteca streamlit e mudanca de variavel

--- data/main.py
+++ data/main.py
@@ -1,12 +1,13 @@
 ﻿import pandas as pd
 import numpy as np
+import streamlit as st
 
 def load_data():
     return pd.read_csv("data\processed\bikes_completed")
 
 def main():
-    df = load_data()
-    st.dataframe( df )
+    df_raw = load_data()
+    st.dataframe( df_raw )
 
 if __name__=="__main__":
     main()
